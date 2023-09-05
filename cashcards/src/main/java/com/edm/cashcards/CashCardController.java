7a18d8e99e6b9938d4755b37de5ce8bc5f946f03
matiaselve4991 <matiaselve4991@gmail.com>
2023-09-05 18:47:54 +0800

    Add repositories and Spring Data

--- cashcards/src/main/java/com/edm/cashcards/CashCardController.java
+++ cashcards/src/main/java/com/edm/cashcards/CashCardController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 /*
     This tells Spring that this class is a Component of type
     RestController and capable of handling HTTP requests.
@@ -17,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @RequestMapping("/cashCard")
 public class CashCardController {
+    private CashCardRepository cashCardRepository;
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
         if (requestedId.equals(77L)) {
             CashCard cashCard = new CashCard(77L, 777.00);
             return ResponseEntity.ok(cashCard);
--- cashcards/src/main/java/com/edm/cashcards/CashCardsApplication.java
+++ cashcards/src/main/java/com/edm/cashcards/CashCardsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashCardsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CashCardsApplication.class, args);
 	}
 
